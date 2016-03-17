e5c85f175973ff3efc3cf246e9d9d638c32b5c20
Giuliano <giuliano.iorio@unibo.it>
2016-03-17 19:48:02 +0100

    Fix cython problems

--- fermi/tsintegrator.py
+++ fermi/tsintegrator.py
@@ -1,3 +1,9 @@
-from .src.intcore import Tsintegrator
-from .src.intcore import Tsintegrator1D
-from .src.intcore import Tsintegrator2D
+try:
+    import cython
+    from .src.intcore import Tsintegrator
+    from .src.intcore import Tsintegrator1D
+    from .src.intcore import Tsintegrator2D
+except:
+    from .src.intcore_py import Tsintegrator
+    from .src.intcore_py import Tsintegrator1D
+    from .src.intcore_py import Tsintegrator2D
